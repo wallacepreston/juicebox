137959f3f1507ed89fb777e0bdcaa450f5f7d741
Preston Wallace <wallace.preston@gmail.com>
2020-05-16 08:05:47 -0700

    Part 2 Day 1 - Collecting Common Middleware

---
+++ index.js
@@ -0,0 +1,14 @@
+const { PORT = 3000 } = process.env;
+const express = require('express');
+const server = express();
+
+server.use((req, res, next) => {
+  console.log('<___Body Logger START___>');
+  console.log(req.body);
+  console.log('<___Body Logger END___>');
+  next();
+});
+
+server.listen(PORT, () => {
+  console.log('Server listening! Juice is flowing on PORT', PORT);
+});
