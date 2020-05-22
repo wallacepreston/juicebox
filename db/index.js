c94cb9a4d5f620f01d8ecc5a646f85b34e50d117
Preston Wallace <wallace.preston@gmail.com>
2020-05-22 15:08:43 -0700

    edit heroku db connection

--- db/index.js
+++ db/index.js
@@ -1,6 +1,10 @@
 const { Client } = require('pg');
 
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev'
+console.log('>>>>>>>>> connectionString', connectionString);
+console.log('>>>>>>>>> process.env.DATABASE_URL', process.env.DATABASE_URL);
+
+const client = new Client(connectionString);
 
 // -------------------- USERS --------------------
 async function getAllUsers() {
