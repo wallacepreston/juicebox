5da0d362fe8e152293db3412ba2398b940c7a18d
Preston Wallace <wallace.preston@gmail.com>
2021-02-20 12:52:58 -0800

    edit connection string

--- db/index.js
+++ db/index.js
@@ -1,8 +1,9 @@
 const { Client } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev'
-
-const client = new Client(connectionString);
+const client = new Client({
+  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
 
 // -------------------- USERS --------------------
 async function getAllUsers() {
